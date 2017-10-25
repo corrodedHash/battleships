b7b0731e9a1d3a3f3bf4f290c17bb45124c9ce32
corrodedHash <corrodedHash@users.noreply.github.com>
2017-10-25 09:35:48 +0200

    Fixed bug

--- battleshell.py
+++ battleshell.py
@@ -21,19 +21,21 @@ class BattleShell(cmd.Cmd):
         self.manager = shotmanager.ShotManager(self.field)
 
     def shoot(self, arg, char):
-        try:
-            coord = util.Coord(alphanum=arg)
-        except RuntimeError:
-            print("given index was probably not correct")
-            return
+        for given_coord in arg.split():
+            try:
+                coord = util.Coord(alphanum=given_coord)
+            except RuntimeError:
+                print("given index was probably not correct")
+                continue
 
-        if coord not in self.field.size:
-            print("index out of range")
-            return
-        if self.field.cells[coord.x][coord.y] == 0:
-            self.field.cells[coord.x][coord.y] = char 
-        else:
-            print("Cell not empty")
+            if coord not in self.field.size:
+                print("index out of range")
+                continue
+            if self.field.cells[coord.x][coord.y] == 0:
+                self.field.cells[coord.x][coord.y] = char 
+            else:
+                print("Cell not empty")
+                continue
 
     def do_suspect(self, arg):
         self.shoot(arg, "~")
@@ -45,8 +47,32 @@ class BattleShell(cmd.Cmd):
         self.shoot(arg, "_")
 
     def do_reset(self, arg):
-        x, y = map(int, arg.split())
-        self.field.cells[x][y] = 0 
+        try:
+            coord = util.Coord(alphanum=arg)
+        except RuntimeError:
+            print("given index was probably not correct")
+            return
+        self.field.cells[coord.x][coord.y] = 0 
+
+    def do_add(self, arg):
+        try:
+            count = int(arg)
+            self.manager.finder.shipcount[count] += 1
+        except ValueError:
+            print("Not a number")
+        except IndexError:
+            print("Not a ship")
+        print(self.manager.finder.shipcount)
+
+    def do_sink(self, arg):
+        try:
+            count = int(arg)
+            self.manager.finder.shipcount[count] -= 1
+        except ValueError:
+            print("Not a number")
+        except IndexError:
+            print("Not a ship")
+        print(self.manager.finder.shipcount)
     
     def postcmd(self, stop, line):
         if self.manager is not None:
