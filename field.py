7a7b9915f3b591f1df1ce5c62c437ba76affcc10
corrodedHash <corrodedHash@users.noreply.github.com>
2017-10-26 22:31:46 +0200

    linting

--- field.py
+++ field.py
@@ -1,11 +1,14 @@
+"""Contains Field class"""
 import copy
-import itertools
 from enum import Enum
 
-from util import Size, Coord, Space, toAlpha 
+from util import Size, Coord, Space, toAlpha
+
 
 class Field:
+    """Battleship field"""
     class States(Enum):
+        """Possible states a battleship field cell can be in"""
         empty = 0
         miss = 1
         hit = 2
@@ -15,15 +18,16 @@ class Field:
         assert size.width > 0
         assert size.height > 0
         self.size = size
-        self.cells = Field.generateField(self.size)
+        self.cells = Field.generate_field(self.size)
 
     @staticmethod
-    def generateField(size: Size):
-        result = [[Field.States.empty for _ in range(size.height)] for _ in range(size.width)]
+    def generate_field(size: Size):
+        """Generates a 2D list to access all cells of the field"""
+        result = [[Field.States.empty for _ in range(
+            size.height)] for _ in range(size.width)]
         return result
 
     def getMargins(self, cell: Coord):
-        tmp_result = list() 
         result = Space()
         for direction in Space.Direction:
             dirTuple = Space.tupleDirMap[direction]
@@ -33,41 +37,36 @@ class Field:
                 new_point = new_point + dirTuple
                 count += 1
             result[direction] = count
-         
+
         return result
-    
+
     def __getitem__(self, key):
-        if type(key) is Coord:
+        if isinstance(key, Coord):
             return self.cells[key.x][key.y]
         else:
             raise TypeError
 
     def __setitem__(self, key, value):
-        if type(key) is Coord:
+        if isinstance(key, Coord):
             self.cells[key.x][key.y] = value
         else:
             raise TypeError
 
-
     def allCells(self):
-        for x in range(self.size.width):
-            for y in range(self.size.height):
-                yield Coord(x, y)
+        """Returns a generator to access all cells of the field"""
+        yield (Coord(x, y) for x in range(self.size.width) for y in range(self.size.height))
 
     def printTable(self, char_fun=lambda board, x, y: board.cells[x][y]):
         result = "  "
-        for x in range(len(self.cells)):
-            result += "| {:<2}".format(x + 1) 
+        result += "".join(["| {:<2}".format(x + 1) for x in range(len(self.cells))])
         result += "\n--"
-        for x in range(len(self.cells)):
-            result += "+---" 
+        result += "+---" * len(self.cells)
         result += "\n"
         for y in range(len(self.cells[0])):
             result += "{0} ".format(toAlpha(y))
             for x in range(len(self.cells)):
-                result += "|{:^3}".format(char_fun(self, x, y)) 
+                result += "|{:^3}".format(char_fun(self, x, y))
             result += "\n--"
-            for x in range(len(self.cells)):
-                result += "+---" 
+            result += "+---" * len(self.cells)
             result += "\n"
         return result
